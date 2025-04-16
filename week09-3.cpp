b9a3074796e3f0f416e1bbc576a524f7c52d0e19
yeh0812 <yehyishan0812@gmail.com>
2025-04-16 12:06:56 +0800

    week09

---
+++ week09-3.cpp
@@ -0,0 +1,15 @@
+//week09-3.cpp
+//YEH001_Star
+#include <stdio.h>
+int main()
+{
+	int n;
+	scanf("%d", &n);
+
+	for(int i=1;i<=n;i++){
+		for(int j=0;j<i;j++){
+			printf("*");
+		}
+		printf("\n",i);
+	}
+}
